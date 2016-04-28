9075403cde7754d1e5e32ad045f04f68c383894d
Dianna Harter <dharter@mynewplace.com>
2016-04-28 16:35:12 -0700

    7's and 10's using while loop in loop-control.php

--- loop-control.php
+++ loop-control.php
@@ -1,20 +1,20 @@
 <?php
 $numbers = range(0, 100, 3);
-for($i = 0; $i < count($numbers); $i++) {
-    if($numbers[$i] > 0 ){
-        if ($numbers[$i] % 7 === 0) {
-            echo("Sevens are lucky, this number has ". ($numbers[$i] / 7) . "\n");
+while (list($key, $number) = each ($numbers)){
+    if($number > 0 ){
+        if ($number % 7 === 0) {
+            echo("Sevens are lucky, this number has ". ($number / 7) . "\n");
 
-            if ($numbers[$i] === (3*7)){
+            if ($number === (3*7)){
                 echo "First Number\n";
-            } elseif ($numbers[$i] === (intdiv(100,3*7)*3*7)) {
+            } elseif ($number === (intdiv(100,3*7)*3*7)) {
                 echo "Last Number\n";
             }
-        } elseif ($numbers[$i] % 10 === 0) {
-            echo("$numbers[$i] is a round number\n");
-            if ($numbers[$i] === (3 * 10)) {
+        } elseif ($number % 10 === 0) {
+            echo("$number is a round number\n");
+            if ($number === (3 * 10)) {
                 echo "First Number\n";
-            } elseif ($numbers[$i] === (intdiv(100,3*10)*3*10)) {
+            } elseif ($number === (intdiv(100,3*10)*3*10)) {
                 echo "Last Number\n";
             }
         }
