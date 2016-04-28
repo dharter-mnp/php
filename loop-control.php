07b68f49dcadd65d315d1b750775116c68f36d72
Dianna Harter <dharter@mynewplace.com>
2016-04-28 15:22:54 -0700

    Iterate loop. Print 3, 9, 15 in loop_control.php

--- loop-control.php
+++ loop-control.php
@@ -1,2 +1,16 @@
 <?php
 $numbers = range(0, 100, 3);
+
+foreach($numbers as $number) {
+    switch ($number) {
+        case (3):
+            echo "Three\n";
+            break;
+        case(9):
+            echo str_repeat("Nine\n",3);
+            break;
+        case(15):
+            echo str_repeat("Fifteen\n", 5);
+            break;
+    }
+}
