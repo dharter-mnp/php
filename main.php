ccdab4de97af22d6cc80f73f271a6cd3898dd205
Dianna Harter <dharter@mynewplace.com>
2016-04-28 14:42:39 -0700

    displaying results with concatenation in main.php

--- main.php
+++ main.php
@@ -3,4 +3,4 @@ require_once ("constants.php");
 
 $array =[TWO => ONE, THREE => 3, FOUR => FIVE];
 $result = $array[TWO] * $array[FOUR] * $array[THREE];
-printf("The result of %d * %d * %d is : %d", $array[TWO] , $array[FOUR] , $array[THREE], $result);
+printf("The result of " . $array[TWO] . " * " . $array[FOUR] . " * " . $array[THREE] . " is : " . $result);
