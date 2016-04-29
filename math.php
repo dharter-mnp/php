29d2881123ece22e4546917392897ccb5d5a93c8
Dianna Harter <dharter@mynewplace.com>
2016-04-29 16:13:08 -0700

    adding doOperation function to math.php

--- math.php
+++ math.php
@@ -8,35 +8,58 @@
  */
 class math
 {
+    const ADD = "Add";
+    const SUBTRACT = "Subtract";
+    const MULTIPLY = "Multiply";
+    const DIVIDE = "Divide";
+    
+    private function doOperation($operation, $value1, $value2, $values){
+        switch ($operation){
+            case (self::ADD):
+                $result = $value1 + $value2;
+                foreach ($values as $value) {
+                    $result += $value;
+                }
+                return $result;
+                break;
+            case self::SUBTRACT:
+                $result = $value1 - $value2;
+                foreach ($values as $value) {
+                    $result -= $value;
+                }
+                return $result;
+                break;
+            case self::MULTIPLY:
+                $result = (int)$value1 * (int)$value2;
+                foreach ($values as $value) {
+                    $result *= (int)$value;
+                }
+                return$result;
+                break;
+            case self::DIVIDE:
+                $result = $value1 / $value2;
+                foreach ($values as $value) {
+                    $result /= $value;
+                }
+                return $result;
+                break;
+                    }
+        
+    }
+    
     public function add($augend, $addend, ...$values){
-        $result = $augend + $addend;
-        foreach ($values as $value) {
-            $result += $value;
-        }
-        return $result;
+        return $this->doOperation(self::ADD, $augend, $addend, $values);
     }
 
     public function subtract($minuend, $subtrahend, ...$values){
-        $result = $minuend - $subtrahend;
-        foreach ($values as $value) {
-            $result -= $value;
-        }
-        return $result;
+        return $this->doOperation(self::SUBTRACT, $minuend, $subtrahend, $values);
     }
 
     public function multiply($multiplicand, $multiplier, ...$values){
-        $result = (int)$multiplicand * (int)$multiplier;
-        foreach ($values as $value) {
-            $result *= (int)$value;
-        }
-        return$result;
+        return $this->doOperation(self::MULTIPLY, $multiplicand, $multiplier, $values);
     }
 
     public function divide($dividend, $divisor, ...$values){
-        $result = $dividend / $divisor;
-        foreach ($values as $value) {
-            $result /= $value;
-        }
-        return $result;
+        return $this->doOperation(self::DIVIDE, $dividend, $divisor, $values);
     }
 }
