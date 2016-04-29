ce27b103af0502217287c4e19f7eeec927481eb1
Dianna Harter <dharter@mynewplace.com>
2016-04-29 09:53:47 -0700

    call functions passing parameters by reference functions.php

--- functions.php
+++ functions.php
@@ -1,18 +1,18 @@
 <?php
 
-function add($augend, $addend){
+function add(&$augend, &$addend){
     return (is_numeric($augend) and is_numeric($addend))? ($augend + $addend) : null;
 }
 
-function subtract($minuend, $subtrahend){
+function subtract(&$minuend, &$subtrahend){
     return (is_numeric($minuend) and is_numeric($subtrahend)) ? $minuend - $subtrahend : null;
 }
 
-function multiply($multiplicand, $multiplier){
+function multiply(&$multiplicand, &$multiplier){
     return (is_numeric($multiplicand) and is_numeric($multiplier)) ? $multiplicand * $multiplier : null;
 }
 
-function divide($dividend, $divisor){
+function divide(&$dividend, &$divisor){
     return (is_numeric($dividend) and is_numeric($divisor) and $divisor != 0) ? $dividend / $divisor : null;
 }
 
